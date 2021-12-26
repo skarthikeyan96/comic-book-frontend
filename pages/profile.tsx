cd1ce98c442404305561e7205e6775b57ce46062
Karthikeyan Shanmugam <karthikeyan@bangthetable.com>
2021-12-26 20:48:00 +0530

    profile and logout component

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,7 +1,55 @@
+import axios from "axios";
+import { useRouter } from "next/router";
+import nookies from "nookies";
+
 import Layout from "../components/Layout";
 
-const Profile = () => {
-  return <Layout></Layout>;
+const Profile = (properties: { user: { email: any; username: any } }) => {
+  const router = useRouter();
+  const {
+    user: { email, username },
+  } = properties;
+
+  const logout = async () => {
+    await axios.get("/api/logout");
+    router.push("/");
+  };
+  return (
+    <Layout>
+      <div>Username: {username}</div>
+      <div>Email: {email}</div>
+      <button onClick={logout}>Logout</button>
+    </Layout>
+  );
+};
+
+export const getServerSideProps = async (context: any) => {
+  const cookies = nookies.get(context);
+  let user;
+
+  if (cookies?.jwt) {
+    const { data } = await axios.get("http://localhost:1337/api/users/me", {
+      headers: {
+        Authorization: `Bearer ${cookies.jwt}`,
+      },
+    });
+    user = data;
+  }
+
+  if (!user) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/",
+      },
+    };
+  }
+
+  return {
+    props: {
+      user,
+    },
+  };
 };
 
 export default Profile;
